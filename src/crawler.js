ee4c279d31423281cdb30133cfdd9bfd76b1d2c8
Phat <daotanp@gmail.com>
2022-10-29 15:57:58 +0700

    .

--- src/crawler.js
+++ src/crawler.js
@@ -36,7 +36,7 @@ async function crawl(...urls) {
 
   const browser = await puppeteer.launch({
     headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
+    args: ['--no-sandbox']
   });
 
   const page = await browser.newPage();
